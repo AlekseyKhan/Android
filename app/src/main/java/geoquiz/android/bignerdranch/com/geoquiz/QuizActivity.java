5a029d14de5b60cdd1c3edda028e833e20a27a93
Aleksey <AlekseyKhankhatov@gmail.com>
2018-02-21 19:41:59 +0800

    Домашняя: добавлен метод gravity для высплывания сообщения сверху экрана. Объявление Тоаст вынесено в основной класс.

--- app/src/main/java/geoquiz/android/bignerdranch/com/geoquiz/QuizActivity.java
+++ app/src/main/java/geoquiz/android/bignerdranch/com/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package geoquiz.android.bignerdranch.com.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +21,19 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this,R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                toast = Toast.makeText(QuizActivity.this,R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,0);
+                toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                toast = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,0);
+                toast.show();
             }
         });
     }
